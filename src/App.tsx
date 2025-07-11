e283834cb37c9b5477685b9cee98ffd79ee8952e
matthew <yongmin@crabit.co.kr>
2025-07-11 13:13:13 +0900

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ function App() {
   return (
     <div>
       <h1>Hello World!!</h1>
-      <h2>YongCoding2</h2>
-      <h3>YongCoding3</h3>
-      <h4>Hyewons 작업</h4>
-      <h5>YongCoding5</h5>
     </div>
   );
 }
