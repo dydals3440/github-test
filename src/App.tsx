b3f2ace2dfea12a7c708b51ef888a9e7196ba792
matthew <yongmin@crabit.co.kr>
2025-07-11 15:30:14 +0900

    feat: clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
 function App() {
-  return (
-    <div>
-      <h1>Hello World!!</h1>
-      <h2>Feature 1</h2>
-      <h3>Feature 2</h3>
-      <h2>Task 1</h2>
-      <h2>Task 2</h2>
-<<<<<<< HEAD
-      <h2>태스크 3</h2>
-      <h2>태스크 4</h2>
-=======
-      <h2>Task 3</h2>
-<<<<<<< HEAD
->>>>>>> 1e8950f (feat: Task 3)
-=======
-      <h2>Task 4</h2>
->>>>>>> 3917e01 (feat: Task 4)
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
